9b8ea9eedf866e5e71f21b9ed055893c7d899348
sudgaura <sudgaura@cisco.com>
2024-03-14 21:05:42 +0530

    Array Proj create

---
+++ array/src/com/exampole/test/TestArray.java
@@ -0,0 +1,19 @@
+package com.exampole.test;
+
+import java.util.Iterator;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+
+		int[] arr = new int[5];
+		
+		int[] arr2 = {1,2,3,4};
+		for (int i = 0; i < arr2.length; i++) {
+			System.out.println("Arr Value ia : "+arr2[i]);
+		}
+		
+		
+	}
+
+}
